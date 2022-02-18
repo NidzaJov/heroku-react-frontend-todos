e3491d30ebc199f679921b242b62480c91c53b27
Nikola Jovanović <nidza.jov@gmail.com>
2022-02-18 17:51:16 +0100

    Small correction in order to make heroku backend app work

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const config = process.env.NODE_ENV === 'production'? {API_URL: 'https://heroku-node-backend-nidza.herokuapp.com/'} : {
+export const config = process.env.NODE_ENV === 'production'? {API_URL: 'https://heroku-node-backend-nidza.herokuapp.com'} : {
     API_URL: 'http://app.test:5000/api'
 };
