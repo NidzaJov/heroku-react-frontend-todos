5772113bd0d3a1e79db7320836164abfeba8ef25
Nikola Jovanović <nidza.jov@gmail.com>
2022-02-22 20:02:48 +0100

    Changing API_URL in config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const config = process.env.NODE_ENV === 'production'? {API_URL: 'https://heroku-node-backend-nidza.herokuapp.com/api'} : {
-    API_URL: 'http://app.test:5000/api'
+    API_URL: 'http://localhost:5000/api'
 };
